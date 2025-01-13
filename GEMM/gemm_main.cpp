196d5e520782caf8cf388e3c138eb55cf1203646
i-am-nobody-1004 <iamnobody1004@outlook.com>
2025-01-13 22:39:12 +0800

    update stage2 gemm

--- GEMM/gemm_main.cpp
+++ GEMM/gemm_main.cpp
@@ -21,7 +21,7 @@
 
 using namespace std;
 #define repeat 200
-#define warmup 10
+#define warmup 20
 
 template <typename AType, typename BType, typename CType>
 void BmFN(int M, int K, int N, int kernel, bool func, bool record){
@@ -45,6 +45,7 @@ void BmFN(int M, int K, int N, int kernel, bool func, bool record){
                 for(int k = 0; k < K; k++){
                     h_Output_Value[m*N+n] += (float)h_A_Value[m*K+k] * (float)h_B_Value[n*K+k];
                 }
+                
             }
         }
     }// end compute output in cpu
@@ -55,16 +56,16 @@ void BmFN(int M, int K, int N, int kernel, bool func, bool record){
     CType * d_Output_Value;
     checkCuda(cudaMalloc(&d_A_Value, (M*K) * sizeof(AType)));
     checkCuda(cudaMalloc(&d_B_Value, (N*K) * sizeof(BType)));
-    checkCuda(cudaMalloc(&d_Output_Value, (M*K) * sizeof(CType)));
+    checkCuda(cudaMalloc(&d_Output_Value, (M*N) * sizeof(CType)));
     // copy value into gpu
     checkCuda(cudaMemcpy(d_A_Value, h_A_Value, (M*K) * sizeof(AType), cudaMemcpyHostToDevice));
-    checkCuda(cudaMemcpy(d_B_Value, h_B_Value, (M*K) * sizeof(BType), cudaMemcpyHostToDevice));
+    checkCuda(cudaMemcpy(d_B_Value, h_B_Value, (N*K) * sizeof(BType), cudaMemcpyHostToDevice));
     
-    {// execute gemm
+    if(kernel % 2 == 1){// execute gemm
         float total_msec = 0.0;
         double Gperf = 0;
         for(int i = 0; i < repeat+warmup; i++){
-            float msec = 0;
+            float msec = 0.0;
             cudaEvent_t start;
             cudaEvent_t end;
             checkCuda(cudaEventCreate(&start));
@@ -79,7 +80,7 @@ void BmFN(int M, int K, int N, int kernel, bool func, bool record){
             if(i >= warmup) {total_msec = total_msec + msec;}
         }
         Gperf = ((double)M) * K * N * 2 / 1000/ 1000 / total_msec * repeat;
-        printf("GEMM:  M = %d, K = %d, N = %d\n", M, K, N);
+        printf("GEMM-stage2:  M = %d, K = %d, N = %d\n", M, K, N);
         printf("\033[33mFP8-TC Avager Performance = \033[35m%lf\033[0m \033[33mGFlops\033[0m\n", Gperf);
         if(record){// record performance output into csv file
             std::ofstream outFile;
@@ -89,23 +90,57 @@ void BmFN(int M, int K, int N, int kernel, bool func, bool record){
             if(kernel == 1){
                 line = "fp8_gemm_v1.csv";
             }
-            else if(kernel == 2){
-                line = "fp8_gemm_v2.csv";
-            }
             else if(kernel == 3){
                 line = "fp8_gemm_v3.csv";
-            }
-            else if(kernel == 4){
-                line = "fp8_gemm_v4.csv";
             }
             else if(kernel == 5){
                 line = "fp8_gemm_v5.csv";
             }
-            else if(kernel == 6){
-                line = "fp8_gemm_v6.csv";
-            }
             else if(kernel == 7){
                 line = "fp8_gemm_v7.csv";
+            }
+            output_dir = output_dir + line;
+
+            outFile.open(output_dir, std::ios::app);
+            //-----------------------------------------------------------------------------
+            outFile << M << ',' << K << ',' << N << ',' << Gperf << std::endl;
+            outFile.close();
+        }
+    }
+    else if(kernel % 2 == 0){// execute gemm
+        float total_msec = 0.0;
+        double Gperf = 0;
+        for(int i = 0; i < repeat+warmup; i++){
+            float msec = 0.0;
+            cudaEvent_t start;
+            cudaEvent_t end;
+            checkCuda(cudaEventCreate(&start));
+            checkCuda(cudaEventCreate(&end));
+            checkCuda(cudaEventRecord(start));
+            ////////////////////////////////////////////
+            gemm::GEMM4(M, K, N, d_A_Value, d_B_Value, d_Output_Value);
+            ////////////////////////////////////////////
+            checkCuda(cudaEventRecord(end));
+            checkCuda(cudaEventSynchronize(end));
+            checkCuda(cudaEventElapsedTime(&msec, start, end));
+            if(i >= warmup) {total_msec = total_msec + msec;}
+        }
+        Gperf = ((double)M) * K * N * 2 / 1000/ 1000 / total_msec * repeat;
+        printf("GEMM-stage4:  M = %d, K = %d, N = %d\n", M, K, N);
+        printf("\033[33mFP8-TC Avager Performance = \033[35m%lf\033[0m \033[33mGFlops\033[0m\n", Gperf);
+        if(record){// record performance output into csv file
+            std::ofstream outFile;
+            //-------------edit here to change the output file-----------------------------
+            std::string output_dir = "./data/";
+            std::string line;
+            if(kernel == 2){
+                line = "fp8_gemm_v2.csv";
+            }
+            else if(kernel == 4){
+                line = "fp8_gemm_v4.csv";
+            }
+            else if(kernel == 6){
+                line = "fp8_gemm_v6.csv";
             }
             else if(kernel == 8){
                 line = "fp8_gemm_v8.csv";
@@ -118,16 +153,18 @@ void BmFN(int M, int K, int N, int kernel, bool func, bool record){
             outFile.close();
         }
     }
+
+    unsigned int num_errors = 0;
     if(func){//verify
         CType * Output_Value_base = new CType[M*N];
         checkCuda(cudaMemcpy(Output_Value_base, d_Output_Value, M*N * sizeof(CType), cudaMemcpyDeviceToHost));
         // verify result
-        int num_errors = 0;
-        float max_error = 0.0f;
+        float max_error = abs((Output_Value_base[0] - h_Output_Value[0]));
         for(int i = 0; i < M*N; i++){
-            float temp_error = abs((float)Output_Value_base[i] - (float)h_Output_Value[i]);
-            if(max_error > 0.5){
-                num_errors += 1;
+            float temp_error = abs((Output_Value_base[i] - h_Output_Value[i]));
+            // std::cout << Output_Value_base[i] << " " << h_Output_Value[i] << " difference: " << temp_error << std::endl;
+            if(temp_error > 0.25){
+                num_errors = num_errors + 1;
             }
             if(temp_error > max_error){
                 max_error = temp_error;
@@ -137,9 +174,11 @@ void BmFN(int M, int K, int N, int kernel, bool func, bool record){
             printf("Result Verified!\n");
         }
         else{
-            printf("There are %d errors.\n", num_errors);
+            // printf("There are %d errors.\n", num_errors);
+            std::cout << "There are " << num_errors << " errors." << std::endl;
         }
         printf("MAx Error is %f\n", max_error);
+        delete Output_Value_base;
     }
     cudaFree(d_A_Value);
     cudaFree(d_B_Value);
@@ -148,13 +187,14 @@ void BmFN(int M, int K, int N, int kernel, bool func, bool record){
     delete h_A_Value;
     delete h_B_Value;
     delete h_Output_Value;
+    
     printf("end BmFN\n");
 }
 
 void usage(void){
     printf("!!!!!!!\n");
     printf("Input Help!\n");
-    printf("Input Format is  ./gemm_main [dimM] [dimK] [dimN] [kernel] [verify] [reocrd]\n");
+    printf("Input Format is  ./gemm_main [dimM] [dimK] [dimN] [kernel] [verify] [reocrd] [testbench]\n");
     printf("dim: M-K-N in GEMM\n");
     printf("kernel: {0 , 1 }\n");
     printf("verify: {0 not verify result with cpu, 1: verify result}\n");
@@ -164,82 +204,104 @@ void usage(void){
 }
 int main(int argc, char **argv){
 
-    if(argc != 7 || strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0){
+    if(argc != 8 || strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0){
         usage();
     }
     // // Run the benchmark
     // else{
-    const int dimM = std::atoi(argv[1]);//A_rows_num
-    const int dimK = std::atoi(argv[2]);//A_cols_num // B_rows_num
-    const int dimN = std::atoi(argv[3]);//B_cols_num
+    int dimM = std::atoi(argv[1]);//A_rows_num
+    int dimK = std::atoi(argv[2]);//A_cols_num // B_rows_num
+    int dimN = std::atoi(argv[3]);//B_cols_num
     const int kernel = std::atoi(argv[4]);//0:
     const int verify = std::atoi(argv[5]);//0:not verify  1:verify
     const int record = std::atoi(argv[6]);//
-    if(kernel == 1){
-        printf("e4m3 * e4m3 = fp16\n");
-        BmFN<__nv_fp8_e4m3, __nv_fp8_e4m3, half>(dimM, dimK, dimN, kernel, verify, record);
-    }
-    else if(kernel == 2){
-        printf("e4m3 * e4m3 = fp32\n");
-        BmFN<__nv_fp8_e4m3, __nv_fp8_e4m3, float>(dimM, dimK, dimN, kernel, verify, record);
-    }
-    else if(kernel == 3){
-        printf("e5m2 * e5m2 = fp16\n");
-        BmFN<__nv_fp8_e5m2, __nv_fp8_e5m2, half>(dimM, dimK, dimN, kernel, verify, record);
-    }
-    else if(kernel == 4){
-        printf("e5m2 * e5m2 = fp32\n");
-        BmFN<__nv_fp8_e5m2, __nv_fp8_e5m2, float>(dimM, dimK, dimN, kernel, verify, record);
-    }
-    else if(kernel == 5){
-        printf("e4m3 * e5m2 = fp16\n");
-        BmFN<__nv_fp8_e4m3, __nv_fp8_e5m2, half>(dimM, dimK, dimN, kernel, verify, record);
-    }
-    else if(kernel == 6){
-        printf("e4m3 * e5m2 = fp32\n");
-        BmFN<__nv_fp8_e4m3, __nv_fp8_e5m2, float>(dimM, dimK, dimN, kernel, verify, record);
-    }
-    else if(kernel == 7){
-        printf("e5m2 * e4m3 = fp16\n");
-        BmFN<__nv_fp8_e5m2, __nv_fp8_e4m3, half>(dimM, dimK, dimN, kernel, verify, record);
-    }
-    else if(kernel == 8){
-        printf("e5m2 * e4m3 = fp32\n");
-        BmFN<__nv_fp8_e5m2, __nv_fp8_e4m3, float>(dimM, dimK, dimN, kernel, verify, record);
+    const int testbench = std::atoi(argv[7]);//
+    if(testbench == 0){
+        if(kernel == 1){
+            printf("e4m3 * e4m3 = fp16\n");
+            BmFN<__nv_fp8_e4m3, __nv_fp8_e4m3, float>(dimM, dimK, dimN, kernel, verify, record);
+        }
+        else if(kernel == 2){
+            printf("e4m3 * e4m3 = fp32\n");
+            BmFN<__nv_fp8_e4m3, __nv_fp8_e4m3, float>(dimM, dimK, dimN, kernel, verify, record);
+        }
+        else if(kernel == 3){
+            printf("e5m2 * e5m2 = fp16\n");
+            BmFN<__nv_fp8_e5m2, __nv_fp8_e5m2, float>(dimM, dimK, dimN, kernel, verify, record);
+        }
+        else if(kernel == 4){
+            printf("e5m2 * e5m2 = fp32\n");
+            // BmFN<__nv_fp8_e5m2, __nv_fp8_e5m2, float>(dimM, dimK, dimN, kernel, verify, record);
+        }
+        else if(kernel == 5){
+            printf("e4m3 * e5m2 = fp16\n");
+            BmFN<__nv_fp8_e4m3, __nv_fp8_e5m2, float>(dimM, dimK, dimN, kernel, verify, record);
+        }
+        else if(kernel == 6){
+            printf("e4m3 * e5m2 = fp32\n");
+            // BmFN<__nv_fp8_e4m3, __nv_fp8_e5m2, flo   at>(dimM, dimK, dimN, kernel, verify, record);
+        }
+        else if(kernel == 7){
+            printf("e5m2 * e4m3 = fp16\n");
+            BmFN<__nv_fp8_e5m2, __nv_fp8_e4m3, float>(dimM, dimK, dimN, kernel, verify, record);
+        }
+        else if(kernel == 8){
+            printf("e5m2 * e4m3 = fp32\n");
+            // BmFN<__nv_fp8_e5m2, __nv_fp8_e4m3, float>(dimM, dimK, dimN, kernel, verify, record);
+        }
+        else{
+            printf("unsupported kernel\n");
+        }
     }
     else{
-        printf("unsupported kernel\n");
+        const int test_num = 64;
+        // int M_t[test_num];
+        // int K_t[test_num];
+        // int N_t[test_num];
+        for(int i = 0; i < test_num; i++){
+            // M_t[i] = 256*(i+1);
+            // K_t[i] = 256*(i+1);
+            // N_t[i] = 256*(i+1);
+            dimM = 256*(i+1);
+            dimK = 256*(i+1);
+            dimN = 256*(i+1);
+
+            if(kernel == 1){
+                printf("e4m3 * e4m3 = fp16\n");
+                BmFN<__nv_fp8_e4m3, __nv_fp8_e4m3, float>(dimM, dimK, dimN, kernel, verify, record);
+            }
+            else if(kernel == 2){
+                printf("e4m3 * e4m3 = fp32\n");
+                BmFN<__nv_fp8_e4m3, __nv_fp8_e4m3, float>(dimM, dimK, dimN, kernel, verify, record);
+            }
+            else if(kernel == 3){
+                printf("e5m2 * e5m2 = fp16\n");
+                BmFN<__nv_fp8_e5m2, __nv_fp8_e5m2, float>(dimM, dimK, dimN, kernel, verify, record);
+            }
+            else if(kernel == 4){
+                printf("e5m2 * e5m2 = fp32\n");
+                // BmFN<__nv_fp8_e5m2, __nv_fp8_e5m2, float>(dimM, dimK, dimN, kernel, verify, record);
+            }
+            else if(kernel == 5){
+                printf("e4m3 * e5m2 = fp16\n");
+                BmFN<__nv_fp8_e4m3, __nv_fp8_e5m2, float>(dimM, dimK, dimN, kernel, verify, record);
+            }
+            else if(kernel == 6){
+                printf("e4m3 * e5m2 = fp32\n");
+                // BmFN<__nv_fp8_e4m3, __nv_fp8_e5m2, float>(dimM, dimK, dimN, kernel, verify, record);
+            }
+            else if(kernel == 7){
+                printf("e5m2 * e4m3 = fp16\n");
+                BmFN<__nv_fp8_e5m2, __nv_fp8_e4m3, float>(dimM, dimK, dimN, kernel, verify, record);
+            }
+            else if(kernel == 8){
+                printf("e5m2 * e4m3 = fp32\n");
+                // BmFN<__nv_fp8_e5m2, __nv_fp8_e4m3, float>(dimM, dimK, dimN, kernel, verify, record);
+            }
+            else{
+                printf("unsupported kernel\n");
+            }
+        }
     }
-
-    // {
-    //     // Open the benchmark file
-    //     std::ifstream infile(paths, std::ifstream::in);
-    //     std::string line;
-
-    //     std::string benchmark;
-
-    //     if(vec_length == 8){
-    //         benchmark = "../benchmark/dlmc/";
-    //     }
-    //     else if(vec_length == 16){
-    //         benchmark = "../benchmark/dlmc-v16/";
-    //     }
-    //     else{
-    //         benchmark = "../benchmark/dlmc/";
-    //     }
-
-    //     int file_count = 0;
-    //     while (std::getline(infile, line)){
-    //         std::string benchmark1;
-    //         benchmark1 = benchmark + line;
-    //         //printf("path: %s\n", line);
-    //         std::cout << "NO." << file_count << "  matrix:" << std::endl;
-    //         if (mixed) BmFN<half, half, int, float, int>(dimM, dimK, dimN, kernel, verify, record);
-    //         else BmFN<half, float, int, float, int>(dimM, dimK, dimN, kernel, verify, record);
-    //         file_count++;
-    //     }
-    //     infile.close();
-    //     printf("matrix num:%d\n", file_count);
-    // }
     return 0;
 }
--- GEMM/include/gemm.cuh
+++ GEMM/include/gemm.cuh
@@ -7,47 +7,52 @@ typedef __nv_fp8_e5m2 e5m2;
 typedef __nv_fp8_e4m3 e4m3;
 
 namespace gemm{
-// original version
-cudaError_t GEMM(int m, int k, int n, 
-    const e5m2* __restrict__ a_matrix,
-    const e5m2* __restrict__ b_matrix,
-    half* __restrict__ output_matrix);
 
-cudaError_t GEMM(int m, int k, int n, 
-    const e5m2* __restrict__ a_matrix,
-    const e5m2* __restrict__ b_matrix,
-    float* __restrict__ output_matrix);
-
-cudaError_t GEMM(int m, int k, int n, 
-    const e4m3* __restrict__ a_matrix,
-    const e4m3* __restrict__ b_matrix,
-    half* __restrict__ output_matrix);
-
+// kernel1
 cudaError_t GEMM(int m, int k, int n, 
     const e4m3* __restrict__ a_matrix,
     const e4m3* __restrict__ b_matrix,
     float* __restrict__ output_matrix);
 
-cudaError_t GEMM(int m, int k, int n, 
+// kernel2
+cudaError_t GEMM4(int m, int k, int n, 
     const e4m3* __restrict__ a_matrix,
-    const e5m2* __restrict__ b_matrix,
-    half* __restrict__ output_matrix);
+    const e4m3* __restrict__ b_matrix,
+    float* __restrict__ output_matrix);
 
+// kernel3
+cudaError_t GEMM(int m, int k, int n, 
+    const e5m2* __restrict__ a_matrix,
+    const e5m2* __restrict__ b_matrix,
+    float* __restrict__ output_matrix);
+
+// kernel4
+cudaError_t GEMM4(int m, int k, int n, 
+    const e5m2* __restrict__ a_matrix,
+    const e5m2* __restrict__ b_matrix,
+    float* __restrict__ output_matrix);
+
+// kernel5
 cudaError_t GEMM(int m, int k, int n, 
     const e4m3* __restrict__ a_matrix,
     const e5m2* __restrict__ b_matrix,
     float* __restrict__ output_matrix);
+// kernel6
+cudaError_t GEMM4(int m, int k, int n, 
+    const e4m3* __restrict__ a_matrix,
+    const e5m2* __restrict__ b_matrix,
+    float* __restrict__ output_matrix);
 
-cudaError_t GEMM(int m, int k, int n, 
-    const e5m2* __restrict__ a_matrix,
-    const e4m3* __restrict__ b_matrix,
-    half* __restrict__ output_matrix);
-
+// kernel7
 cudaError_t GEMM(int m, int k, int n, 
     const e5m2* __restrict__ a_matrix,
     const e4m3* __restrict__ b_matrix,
     float* __restrict__ output_matrix);
-    
+// kernel8
+cudaError_t GEMM4(int m, int k, int n, 
+    const e5m2* __restrict__ a_matrix,
+    const e4m3* __restrict__ b_matrix,
+    float* __restrict__ output_matrix);
 } // namespace gemm
 
 #endif
